8fe11fc592342c29bf5ce74e4076a3f71e2a998b
Jay Rabjohns <52382074+jayrabjohns@users.noreply.github.com>
2023-01-26 22:41:16 +0000

    q2 p2

--- 2022/src/q2.rs
+++ 2022/src/q2.rs
@@ -1,6 +1,14 @@
 use std::fs;
 
 pub fn part_one() {
+    q2(&calc_score_p1);
+}
+
+pub fn part_two() {
+    q2(&calc_score_p2);
+}
+
+fn q2(calc_score: &dyn Fn(&str) -> u32) {
     let file = fs::read_to_string("resources/q2");
     let result = file.map(|contents| {
         contents
@@ -15,9 +23,7 @@ pub fn part_one() {
     }
 }
 
-pub fn part_two() {}
-
-fn calc_score(round: &str) -> u32 {
+fn calc_score_p1(round: &str) -> u32 {
     match round {
         "A X" => 4,
         "A Y" => 8,
@@ -28,6 +34,21 @@ fn calc_score(round: &str) -> u32 {
         "C X" => 7,
         "C Y" => 2,
         "C Z" => 6,
+        undefined => panic!("Cannot parse invalid round `{undefined}`."),
+    }
+}
+
+fn calc_score_p2(round: &str) -> u32 {
+    match round {
+        "A X" => 3,
+        "A Y" => 4,
+        "A Z" => 8,
+        "B X" => 1,
+        "B Y" => 5,
+        "B Z" => 9,
+        "C X" => 2,
+        "C Y" => 6,
+        "C Z" => 7,
         undefined => panic!("Cannot parse invalid round `{undefined}`."),
     }
 }
