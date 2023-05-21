3f84f8656f45f7a881677c92f0d7a3ba9740256e
Jay Rabjohns <52382074+jayrabjohns@users.noreply.github.com>
2023-05-21 10:26:21 +0100

    feat!: use clap for argument passing & change syntax for calling exe

--- 2022/src/main.rs
+++ 2022/src/main.rs
@@ -1,51 +1,30 @@
 mod questions;
 
+use clap::Parser;
 use questions::{Question, Q1, Q2, Q3, Q4, Q5, Q6};
-use regex::Regex;
-use std::env;
+
+#[derive(Parser)]
+struct Args {
+    /// Day number - int in range [1,25]
+    day: usize,
+}
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    if !validate_args(&args) {
-        print_usage();
-        return;
-    }
-
-    let input = args[1].as_str();
-    let questions: Vec<(&str, Box<dyn Question>)> = vec![
-        ("q1", Box::new(Q1)),
-        ("q2", Box::new(Q2)),
-        ("q3", Box::new(Q3)),
-        ("q4", Box::new(Q4)),
-        ("q5", Box::new(Q5)),
-        ("q6", Box::new(Q6)),
+    let args = Args::parse();
+    let questions: Vec<Box<dyn Question>> = vec![
+        Box::new(Q1),
+        Box::new(Q2),
+        Box::new(Q3),
+        Box::new(Q4),
+        Box::new(Q5),
+        Box::new(Q6),
     ];
-    questions
-        .iter()
-        .find(|(q_str, _)| q_str == &input)
-        .map_or_else(
-            || {
-                println!(
-                    "Invalid question number. There are {} questions",
-                    questions.len()
-                );
-            },
-            |(_, question)| question.run_all(),
-        );
-}
 
-fn validate_args(args: &Vec<String>) -> bool {
-    let question_regex = Regex::new(r"^q\d+$").unwrap();
-    if args.len() > 0 && question_regex.is_match(&args[1]) {
-        return true;
-    }
-
-    return false;
-}
-
-fn print_usage() {
-    println!("advent-of-code-2022 <question>");
-    println!("| where <question> is of the form q<question number>.");
-    println!("| E.g. advent-of-code-2022 q1");
-    println!("| This will run part one and part two of the question (if one exists).");
+    questions.get(args.day - 1).map_or_else(
+        || println!("Invalid day number. There are {} days", questions.len()),
+        |question| {
+            println!("Running solutions for day {}", args.day);
+            question.run_all();
+        },
+    );
 }
--- README.md
+++ README.md
@@ -4,7 +4,11 @@
 
 Written in rust.
 
-### Running locally 
-```shell
-cargo run --release --time -- <question number>
-```
+### Usage
+Usage: advent-of-code-2022 <DAY>
+
+Arguments:
+  <DAY>  Day number - int in range [1,25]
+
+Options:
+  -h, --help  Print help
