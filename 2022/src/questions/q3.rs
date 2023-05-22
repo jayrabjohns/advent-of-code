d2271cad8fcfbbfc5ca014b7618c5f85ba1bb5e8
Jay Rabjohns <52382074+jayrabjohns@users.noreply.github.com>
2023-05-22 18:37:14 +0100

    refactor: q3 to use result

--- 2022/src/questions/q3.rs
+++ 2022/src/questions/q3.rs
@@ -1,94 +1,76 @@
-use std::{
-    collections::HashSet,
-    io::{self, BufRead, BufReader},
-};
+use std::{collections::HashSet, error::Error, fs, io};
 
 use super::Question;
 
 pub struct Q3;
 impl Question for Q3 {
-    fn part_one(&self) {
-        let file = std::fs::File::open("resources/q3").map(|file| BufReader::new(file));
-        let result = file.map(|reader| {
-            reader
-                .lines()
-                .map(|line| line.map(calc_score_p1))
-                .collect::<Result<Vec<_>, _>>()
-                .map(|vec| vec.iter().sum::<u32>())
-        });
+    fn part_one(&self) -> Result<String, Box<dyn Error>> {
+        let contents = fs::read_to_string("resources/q3")?;
+        let priorities: Option<Vec<u32>> = contents
+            .lines()
+            .map(&split_string_in_half)
+            .map(|(left, right)| {
+                let left_set: HashSet<char> = left.chars().collect();
+                right
+                    .chars()
+                    .filter(|c| left_set.contains(c))
+                    .next()
+                    .map(|char| get_priority(&char))
+            })
+            .collect();
 
-        match result {
-            Err(err) => panic!("{err}"),
-            Ok(Err(err)) => panic!("{err}"),
-            Ok(Ok(sum)) => println!("Sum of priorities: {sum}"),
+        match priorities {
+            Some(priorities) => Ok(priorities.iter().sum::<u32>().to_string()),
+            None => Err(Box::new(io::Error::new(
+                io::ErrorKind::InvalidData,
+                "Empty file",
+            ))),
         }
     }
 
-    fn part_two(&self) {
-        let file = std::fs::File::open("resources/q3").map(|file| BufReader::new(file));
-        let result: Result<u32, io::Error> = file.map(|reader| {
-            reader
-                .lines()
-                .collect::<Vec<_>>()
-                .chunks(3)
-                .map(|chunk| {
-                    let chunk = chunk.iter().map(|line| match line {
-                        Err(err) => panic!("{err}"),
-                        Ok(str) => str,
-                    });
-                    find_common_char(chunk)
-                        .map(|possibilites| get_priority(possibilites[0]))
-                        .unwrap()
-                        .unwrap()
-                })
-                .sum()
-        });
+    fn part_two(&self) -> Result<String, Box<dyn Error>> {
+        let contents = fs::read_to_string("resources/q3")?;
+        let lines: Vec<&str> = contents.lines().collect();
+        let sum_priorities: u32 = lines
+            .chunks(3)
+            .map(&find_common_char)
+            .map(|chars| chars.iter().map(&get_priority).sum::<u32>())
+            .sum();
 
-        match result {
-            Err(err) => panic!("{err}"),
-            Ok(sum) => println!("Sum of priorities: {sum}"),
-        }
+        Ok(sum_priorities.to_string())
     }
 }
 
-fn find_common_char<'a>(chunk: impl Iterator<Item = &'a String>) -> Option<Vec<char>> {
-    let mut chunk = chunk;
-    let mut possibilities: Option<Vec<char>> = chunk.next().map(|line| line.chars().collect());
-    chunk.for_each(|line| {
-        possibilities = possibilities
-            .as_ref()
-            .map(|chars| line.chars().filter(|char| chars.contains(char)).collect());
-    });
-
-    return possibilities;
-}
-
-fn calc_score_p1(line: String) -> u32 {
-    let mid_point = line.len() / 2;
-    let left = &line[0..mid_point];
-    let right = &line[mid_point..];
-    let left_set: HashSet<char> = left.chars().collect();
-    return right
-        .chars()
-        .filter(|c| left_set.contains(&c))
+fn find_common_char<'a>(chunk: &[&str]) -> Vec<char> {
+    let mut chunk = chunk.iter();
+    chunk
         .next()
-        .map(get_priority)
-        .unwrap()
-        .unwrap();
+        .map(|&line| line.chars().collect::<Vec<_>>())
+        .map(|mut possibilities| {
+            chunk.for_each(|&line| {
+                possibilities = line
+                    .chars()
+                    .filter(|char| possibilities.contains(char))
+                    .collect()
+            });
+            possibilities
+        })
+        .unwrap_or(vec![])
 }
 
-fn get_priority(char: char) -> Option<u32> {
+fn split_string_in_half<'a>(str: &'a str) -> (&'a str, &'a str) {
+    let mid_point = str.len() / 2;
+    (&str[0..mid_point], &str[mid_point..])
+}
+
+fn get_priority(char: &char) -> u32 {
     if !char.is_ascii_alphabetic() {
-        return None;
+        return 0;
     }
 
-    let priority = if char.is_uppercase() {
-        (char as u32) - ('A' as u32) + 26
+    (if char.is_uppercase() {
+        (*char as u32) - ('A' as u32) + 26
     } else {
-        (char as u32) - ('a' as u32)
-    } + 1;
-
-    // println!("Priority for {char} = {priority}");
-
-    return Some(priority);
+        (*char as u32) - ('a' as u32)
+    }) + 1
 }
