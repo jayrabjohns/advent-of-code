b189f6b39bae29adee7c1739294fb1e50798f1ec
Jay Rabjohns <52382074+jayrabjohns@users.noreply.github.com>
2023-05-23 18:05:50 +0100

    refactor: q4 to return result

--- 2022/src/questions/q4.rs
+++ 2022/src/questions/q4.rs
@@ -1,5 +1,4 @@
-use std::io::BufRead;
-use std::{fs, io::BufReader};
+use std::{error::Error, fs};
 
 use super::Question;
 
@@ -10,16 +9,16 @@ struct Range {
 
 pub struct Q4;
 impl Question for Q4 {
-    fn part_one(&self) {
-        q4(&|l: Range, r: Range| {
+    fn part_one(&self) -> Result<String, Box<dyn Error>> {
+        q4(&|l: &Range, r: &Range| {
             let l_contains_r = l.lower_bound <= r.lower_bound && l.upper_bound >= r.upper_bound;
             let r_contains_l = l.lower_bound >= r.lower_bound && l.upper_bound <= r.upper_bound;
             l_contains_r || r_contains_l
         })
     }
 
-    fn part_two(&self) {
-        q4(&|l: Range, r: Range| {
+    fn part_two(&self) -> Result<String, Box<dyn Error>> {
+        q4(&|l: &Range, r: &Range| {
             let l_left_of_r = l.upper_bound < r.lower_bound;
             let l_right_of_r = l.lower_bound > r.upper_bound;
             !(l_left_of_r || l_right_of_r)
@@ -27,35 +26,31 @@ impl Question for Q4 {
     }
 }
 
-fn q4(filter_pred: &dyn Fn(Range, Range) -> bool) {
-    let file = fs::File::open("resources/q4").map(BufReader::new);
-    let result = file.map(|reader| {
-        reader
-            .lines()
-            .map(Result::unwrap)
-            .filter(|line| {
-                let mut split = line.split(',');
+fn q4(filter_pred: &dyn Fn(&Range, &Range) -> bool) -> Result<String, Box<dyn Error>> {
+    let contents = fs::read_to_string("resources/q4")?;
+    let result: Result<Vec<(Range, Range)>, Box<dyn Error>> = contents
+        .lines()
+        .flat_map(|line| {
+            let mut split = line.split(',');
 
-                let mut l_range = split.next().unwrap().split('-');
-                let l = Range {
-                    lower_bound: l_range.next().unwrap().parse().unwrap(),
-                    upper_bound: l_range.next().unwrap().parse().unwrap(),
-                };
+            let mut lhs = split.next()?.split('-');
+            let mut rhs = split.next()?.split('-');
 
-                let mut r_range = split.next().unwrap().split('-');
-                let r = Range {
-                    lower_bound: r_range.next().unwrap().parse().unwrap(),
-                    upper_bound: r_range.next().unwrap().parse().unwrap(),
-                };
+            let l_range = split_to_range(lhs.next()?, lhs.next()?);
+            let r_range = split_to_range(rhs.next()?, rhs.next()?);
 
-                filter_pred(l, r)
-            })
-            .collect::<Vec<_>>()
-            .len()
-    });
+            Some(l_range.and_then(|l| r_range.map(|r| (l, r))))
+        })
+        .collect();
 
-    match result {
-        Err(err) => panic!("{err}"),
-        Ok(len) => println!("Number of pairs: {len}"),
-    }
+    let filtered_results = result?.into_iter().filter(|(l, r)| filter_pred(l, r));
+
+    Ok(filtered_results.count().to_string())
+}
+
+fn split_to_range(lower_bound_str: &str, upper_bound_str: &str) -> Result<Range, Box<dyn Error>> {
+    Ok(Range {
+        lower_bound: lower_bound_str.parse()?,
+        upper_bound: upper_bound_str.parse()?,
+    })
 }
